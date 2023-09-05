0eee037c4b66c50e80dfc3b6303ed7a9ea34d1f3
Crowdbotics <engineering@crowdbotics.com>
2023-09-05 20:37:40 +0000

    Initial commit.

---
+++ backend/test_studio_modal__114381/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for test_studio_modal__114381 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_studio_modal__114381.settings')
+
+application = get_wsgi_application()
